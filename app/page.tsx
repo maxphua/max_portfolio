1f71ee88a1a361bc2344fd31984172936143fc91
Max Phua <maxphua90@gmail.com>
2024-05-30 14:54:41 +0800

    Implement client moving cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
+import Clients from "@/components/Clients";
 import  Grid  from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import Image from "next/image";
+import { navItems } from "@/data";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: "home", link: '/', icon: <FaHome />}
-        ]}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
